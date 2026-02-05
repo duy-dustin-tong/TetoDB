78393fd01fe7e22125a76111b25a1174ae2e23a5
duy-dustin-tong <duy-dustin-tong>
2026-02-05 03:39:02 -0500

    added src/include/storage/page/page.h

--- src/include/storage/page/page.h
+++ src/include/storage/page/page.h
@@ -1,4 +1,40 @@
 // page.h
 
-// Role: A wrapper around a 4KB char array.
+// Role: A wrapper that points a 4KB block in buffer.
 // Exposes: GetData(), GetPageId(), RLatch(), WLatch().
+
+#pragma once
+
+#include <array>
+#include <algorithm>
+#include <iostream>
+#include "common/config.h"
+
+namespace tetodb {
+
+	class Page {
+		friend class BufferPoolManager;
+
+	public:
+		Page() = default;
+		~Page() = default;
+
+		inline char* GetData() { return data_; }
+		inline page_id_t GetPageId() const { return page_id_; }
+		//inline int32_t GetPinCount() { return pin_count_; }
+		inline void ResetMemory() {
+			if (data_) memset(data_, 0, PAGE_SIZE);
+		}
+
+	protected:
+		// meta data
+		page_id_t page_id_{ INVALID_PAGE_ID };
+		//int32_t pin_count_{ 0 };
+		bool is_dirty_{ 0 };
+
+		// actual data address
+		char* data_; // use raw pointer here because memory is allocated in buffer
+
+	};
+
+} // namespace tetodb
